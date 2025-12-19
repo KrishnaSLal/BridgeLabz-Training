637769aa020aebd28565e7af814b9899dcab71bf
Krishna S Lal <krishnaslal0204@gmail.com>
2025-12-19 21:30:08 +0530

    Delete gcr-codebase/programming-elements/LevelOne/Marks.java

--- gcr-codebase/programming-elements/LevelOne/Marks.java
+++
@@ -1,9 +0,0 @@
-public class Marks{
-       public static void main(String[]args){
-	      
-		  int Mathematics = 94, Physics = 95, Chemistry = 96;
-	      float average = (Mathematics+Physics+Chemistry)/3;
-	      
-		  System.out.print("Sam’s average mark in PCM is " + average);
-	   }
-}
