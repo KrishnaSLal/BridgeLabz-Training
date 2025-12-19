6e5e65ea321ea31fab2aa2d40a7686fbbcdd98a4
KrishnaSLal <krishnaslal0204@gmail.com>
2025-12-19 20:51:31 +0530

    [KrishnaSLal]: Added multiples below 100 using while loop

---
+++ gcr-codebase/control-flow/Level-2/MultiplesBelow100While.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MultiplesBelow100While {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer less than 100: ");
+        int number = input.nextInt();
+
+        
+        if (number <= 0 || number >= 100) {
+            System.out.println("Please enter a positive integer less than 100.");
+        } else {
+            System.out.println("Multiples of " + number + " below 100 are:");
+
+            int counter = 100; // Start from 100
+
+            // backward looping
+            while (counter >= 1) {
+                if (counter % number == 0) {
+                    System.out.println(counter);
+                }
+                counter--; 
+			}
+        }
+
+        input.close();
+    }
+}
