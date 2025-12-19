049824259d005c116c3de6ab297d67d2e5572481
KrishnaSLal <krishnaslal0204@gmail.com>
2025-12-19 21:23:00 +0530

    [KrishnaSLal]: Added marks programming

---
+++ gcr-codebase/programming-elements/LevelOne/Marks.java
@@ -0,0 +1,9 @@
+public class Marks{
+       public static void main(String[]args){
+	      
+		  int Mathematics = 94, Physics = 95, Chemistry = 96;
+	      float average = (Mathematics+Physics+Chemistry)/3;
+	      
+		  System.out.print("Sam’s average mark in PCM is " + average);
+	   }
+}
