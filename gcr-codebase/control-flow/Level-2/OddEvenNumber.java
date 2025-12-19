09f9a57ec01b897ca401e48d84ba70aa5fa104aa
KrishnaSLal <krishnaslal0204@gmail.com>
2025-12-19 20:54:18 +0530

    [KrishnaSLal]: Added odd and even number program

---
+++ gcr-codebase/control-flow/Level-2/OddEvenNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class OddEvenNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer: ");
+        int number = input.nextInt();
+
+        // Check if the input is a natural number
+        if (number > 0) {
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is an even number");
+                } else {
+                    System.out.println(i + " is an odd number");
+                }
+            }
+        } else {
+            System.out.println("Please enter a valid natural number (positive integer).");
+        }
+
+        input.close();
+    }
+}
