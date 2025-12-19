23178f57b2882210c698b9a6268507e6f7dfee47
KrishnaSLal <krishnaslal0204@gmail.com>
2025-12-19 21:34:33 +0530

    [KrishnaSLal]: Added calculation of harry's age program

---
+++ gcr-codebase/programming-elements/LevelOne/AgeOfHarry.java
@@ -0,0 +1,10 @@
+public class AgeOfHarry{
+       public static void main(String []args){
+
+          int DateOfBirth = 2000, CurrentYear =  2024;
+	      //Current age of Harry can be calculated by "2024-2000".
+	      int Age =  2024-2000;
+	 
+	     System.out.println("Harry's Age in 2024 is " + Age);
+    }
+}
