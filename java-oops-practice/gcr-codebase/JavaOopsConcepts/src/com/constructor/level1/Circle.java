6d00382f805c3c4f3a2cf3a5dac250208694c9d9
KrishnaSLal <krishnaslal0204@gmail.com>
2025-12-28 12:06:19 +0530

    [KrishnaSLal]: Added circle radius initializing program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/Circle.java
@@ -0,0 +1,32 @@
+package com.constructor.level1;
+
+public class Circle {
+
+	    double radius;
+
+	    // Default constructor
+	    Circle() {
+	        this(1.0);   // calls parameterized constructor
+	    }
+
+	    // Parameterized constructor
+	    Circle(double radius) {
+	        this.radius = radius;
+	    }
+
+	    void display() {
+	        System.out.println("Radius: " + radius);
+	    }
+
+	    public static void main(String[] args) {
+
+	        // Using default constructor
+	        Circle c1 = new Circle();
+	        c1.display();
+
+	        // Using parameterized constructor
+	        Circle c2 = new Circle(5.5);
+	        c2.display();
+	    }
+	}
+
