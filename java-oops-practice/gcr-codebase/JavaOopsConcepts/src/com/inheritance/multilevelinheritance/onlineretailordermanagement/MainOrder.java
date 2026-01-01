e6a817b084e3bb9feed540887336fea201e6777f
KrishnaSLal <krishnaslal0204@gmail.com>
2026-01-01 16:02:56 +0530

    [KrishnaSLal]: Added online retail order management

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevelinheritance/onlineretailordermanagement/MainOrder.java
@@ -0,0 +1,16 @@
+package com.inheritance.multilevelinheritance.onlineretailordermanagement;
+
+public class MainOrder {
+
+    public static void main(String[] args) {
+
+        Order o1 = new Order(101, "01-Jan-2026");
+        Order o2 = new ShippedOrder(102, "02-Jan-2026", "TRK12345");
+        Order o3 = new DeliveredOrder(103, "03-Jan-2026",
+                                      "TRK67890", "06-Jan-2026");
+
+        System.out.println(o1.getOrderStatus());
+        System.out.println(o2.getOrderStatus());
+        System.out.println(o3.getOrderStatus());
+    }
+}
