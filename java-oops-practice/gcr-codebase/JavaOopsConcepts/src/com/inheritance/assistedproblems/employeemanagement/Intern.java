d43e354d7d88f2ace75fc0bfef4ad2069acbcc16
KrishnaSLal <krishnaslal0204@gmail.com>
2026-01-01 15:03:25 +0530

    [KrishnaSLal]: Added employee management system

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/employeemanagement/Intern.java
@@ -0,0 +1,17 @@
+package com.inheritance.assistedproblems.employeemanagement;
+
+class Intern extends Employee {
+
+    int durationInMonths;
+
+    Intern(String name, int id, double salary, int durationInMonths) {
+        super(name, id, salary);
+        this.durationInMonths = durationInMonths;
+    }
+
+    @Override
+    void displayDetails() {
+        super.displayDetails();
+        System.out.println("Internship Duration: " + durationInMonths + " months");
+    }
+}
---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/employeemanagement/MainEmployee.java
@@ -0,0 +1,19 @@
+package com.inheritance.assistedproblems.employeemanagement;
+
+public class MainEmployee {
+
+    public static void main(String[] args) {
+
+        Employee e1 = new Manager("Ravi", 101, 80000, 10);
+        Employee e2 = new Developer("Anita", 102, 60000, "Java");
+        Employee e3 = new Intern("Kiran", 103, 15000, 6);
+
+        e1.displayDetails();
+        System.out.println();
+
+        e2.displayDetails();
+        System.out.println();
+
+        e3.displayDetails();
+    }
+}
