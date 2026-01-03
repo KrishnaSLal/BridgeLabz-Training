54296819164e6e14397d64e924c06877362d178c
KrishnaSLal <krishnaslal0204@gmail.com>
2026-01-03 11:47:48 +0530

    [KrishnaSLal]: Added hospital management system

---
+++ java-oops-practice/scenario-based/JavaOopsScenarioBased/src/hospitalmanagementsystem/MainHospitalManagementApp.java
@@ -0,0 +1,19 @@
+package hospitalmanagementsystem;
+
+public class MainHospitalManagementApp {
+	public static void main(String[] args) {
+
+        Patient p1 = new InPatient(101, "Ravi", "Diabetes", 5);
+        Patient p2 = new OutPatient(102, "Anita");
+
+        Doctor d1 = new Doctor(1, "Dr. Sharma", "Cardiology");
+
+        Bill bill = new Bill(500, 3000, 200);
+
+        p1.displayInfo();   // Polymorphism
+        p2.displayInfo();
+        d1.displayInfo();
+
+        System.out.println("Total Bill Amount: ₹" + bill.calculatePayment());
+    }
+}
