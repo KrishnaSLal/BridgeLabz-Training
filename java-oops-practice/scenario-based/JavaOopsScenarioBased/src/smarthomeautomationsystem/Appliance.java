7ab430ca74564550b3e7851e895db44506d89d56
KrishnaSLal <krishnaslal0204@gmail.com>
2026-01-03 12:21:19 +0530

    [KrishnaSLal]: Added smart home automation

---
+++ java-oops-practice/scenario-based/JavaOopsScenarioBased/src/smarthomeautomationsystem/Appliance.java
@@ -0,0 +1,23 @@
+package smarthomeautomationsystem;
+
+abstract class Appliance implements Controllable {
+    private boolean isOn;
+    private int powerConsumption; // watts
+
+    // Default power setting
+    public Appliance(int powerConsumption) {
+        this.powerConsumption = powerConsumption;
+    }
+
+    protected void setOn(boolean status) {
+        isOn = status;
+    }
+
+    protected boolean isOn() {
+        return isOn;
+    }
+
+    public int getPowerConsumption() {
+        return powerConsumption;
+    }
+}
---
+++ java-oops-practice/scenario-based/JavaOopsScenarioBased/src/smarthomeautomationsystem/MainHomeApp.java
@@ -0,0 +1,20 @@
+package smarthomeautomationsystem;
+
+public class MainHomeApp {
+	public static void main(String[] args) {
+
+        UserController controller = new UserController();
+
+        Appliance light = new Light();
+        Appliance fan = new Fan();
+        Appliance ac = new AC();
+
+        controller.controlDevice(light, true);
+        controller.controlDevice(ac, true);
+
+        controller.compareEnergy(light, ac);
+
+        controller.controlDevice(light, false);
+        controller.controlDevice(ac, false);
+    }
+}
