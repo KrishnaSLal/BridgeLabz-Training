f9e010580ebfc8d63810ab5c68e3af9852e92d11
KrishnaSLal <krishnaslal0204@gmail.com>
2026-01-25 21:51:26 +0530

    [KrishnaSLal]: Added a program to remove duplicates in a list while preserving the order

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/javacollectionframework/removeduplicates/RemoveDuplicates.java
@@ -0,0 +1,32 @@
+package com.javacollectionframework.removeduplicates;
+
+import java.util.*;
+
+public class RemoveDuplicates {
+
+    public static void main(String[]args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Number of elements
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+
+        // Input elements
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+        // Remove duplicates while preserving order
+        Set<Integer> uniqueSet = new LinkedHashSet<>(list);
+        List<Integer> result = new ArrayList<>(uniqueSet);
+        
+        System.out.println("List after removing duplicates:");
+        System.out.println(result);
+
+        sc.close();
+    }
+}
