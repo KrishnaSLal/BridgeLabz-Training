18c4c1b9eac83edf6ccf16d80e1f8a4d3eea86c2
KrishnaSLal <krishnaslal0204@gmail.com>
2026-01-25 21:48:01 +0530

    [KrishnaSLal]: Added a program to rotate the elements in a list

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/javacollectionframework/rotateelement/ElementRotation.java
@@ -0,0 +1,41 @@
+package com.javacollectionframework.rotateelement;
+
+import java.util.*;
+
+public class ElementRotation {
+
+    public static void main(String[]args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Number of elements
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+
+        // input
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+        // Rotation count
+        System.out.print("Enter rotation count: ");
+        int k = sc.nextInt();
+
+        //Inorder to handle the rotation which is greater than the size
+        k = k % n;
+
+        // Rotate left
+        List<Integer> rotatedList = new ArrayList<>();
+
+        rotatedList.addAll(list.subList(k, n));
+        rotatedList.addAll(list.subList(0, k));
+
+        System.out.println("Rotated List:");
+        System.out.println(rotatedList);
+
+        sc.close();
+    }
+}
