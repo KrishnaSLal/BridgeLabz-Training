1dc98b9fadf30006db1005a398d2ae14813d8db3
KrishnaSLal <krishnaslal0204@gmail.com>
2026-01-25 23:05:14 +0530

    [KrishnaSLal]: Added a program to find the symmetric difference

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/javacollectionframework/set/symmetricdifference/SymmetricDifference.java
@@ -0,0 +1,46 @@
+package com.javacollectionframework.set.symmetricdifference;
+
+import java.util.*;
+
+public class SymmetricDifference {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // First set
+        System.out.print("Enter number of elements in Set 1: ");
+        int n1 = sc.nextInt();
+
+        Set<Integer> set1 = new HashSet<>();
+        System.out.println("Enter elements of Set 1:");
+        for (int i = 0; i < n1; i++) {
+            set1.add(sc.nextInt());
+        }
+
+        // Second set
+        System.out.print("Enter number of elements in Set 2: ");
+        int n2 = sc.nextInt();
+
+        Set<Integer> set2 = new HashSet<>();
+        System.out.println("Enter elements of Set 2:");
+        for (int i = 0; i < n2; i++) {
+            set2.add(sc.nextInt());
+        }
+
+        // Union
+        Set<Integer> union = new HashSet<>(set1);
+        union.addAll(set2);
+
+        // Intersection
+        Set<Integer> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+
+        // Symmetric Difference
+        union.removeAll(intersection);
+
+        System.out.println("Symmetric Difference: " + union);
+
+        sc.close();
+    }
+}
