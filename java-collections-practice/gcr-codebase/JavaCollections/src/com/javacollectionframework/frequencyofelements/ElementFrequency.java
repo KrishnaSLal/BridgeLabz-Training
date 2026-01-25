1f299e8978d63d05871b0093d30deb9e0cab6520
KrishnaSLal <krishnaslal0204@gmail.com>
2026-01-25 21:42:50 +0530

    [KrishnaSLal]: Added a program for finding the frequency of elements in a list of strings

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/javacollectionframework/frequencyofelements/ElementFrequency.java
@@ -0,0 +1,36 @@
+package com.javacollectionframework.frequencyofelements;
+
+import java.util.*;
+
+public class ElementFrequency {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // user input  of elements to be stored 
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        List<String> list = new ArrayList<>();
+
+        //input
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextLine());
+        }
+
+        // Frequency map
+        Map<String, Integer> frequencyMap = new HashMap<>();
+
+        for (String item : list) {
+            frequencyMap.put(item, frequencyMap.getOrDefault(item, 0) + 1);
+        }
+
+        System.out.println("Frequency of elements:");
+        System.out.println(frequencyMap);
+
+        sc.close();
+    }
+}
