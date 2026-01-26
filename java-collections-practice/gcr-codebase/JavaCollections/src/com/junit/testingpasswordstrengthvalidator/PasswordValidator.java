8498f8e5ec647f7910bd903ba98f04527b849860
KrishnaSLal <krishnaslal0204@gmail.com>
2026-01-27 00:05:24 +0530

    [KrishnaSLal]: Added testing password strength validator

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/junit/testingpasswordstrengthvalidator/PasswordValidator.java
@@ -0,0 +1,16 @@
+package com.junit.testingpasswordstrengthvalidator;
+
+public class PasswordValidator {
+
+    public boolean isValid(String password) {
+
+        if (password == null || password.length() < 8) {
+            return false;
+        }
+
+        boolean hasUppercase = password.matches(".*[A-Z].*");
+        boolean hasDigit = password.matches(".*\\d.*");
+
+        return hasUppercase && hasDigit;
+    }
+}
