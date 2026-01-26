8706621bab698276b0bea482ca590adb940c866c
KrishnaSLal <krishnaslal0204@gmail.com>
2026-01-26 23:48:04 +0530

    [KrishnaSLal]: Added testing exception handling

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/junit/testingexceptionhandling/MathUtils.java
@@ -0,0 +1,12 @@
+package com.junit.testingexceptionhandling;
+
+public class MathUtils {
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
+    }
+}
+ 
---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/junit/testingexceptionhandling/MathUtilsTest.java
@@ -0,0 +1,5 @@
+package com.junit.testingexceptionhandling;
+
+public class MathUtilsTest {
+
+}
