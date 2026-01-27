cc3e061d50d2507f94b419b3fd634b5219a7ddd3
KrishnaSLal <krishnaslal0204@gmail.com>
2026-01-27 16:06:42 +0530

    [KrishnaSLal]: Added a program to test string utility methods

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/junit/test/StringUtilsTest.java
@@ -0,0 +1,35 @@
+package com.junit.test;
+
+import org.junit.Test;
+
+import com.junit.com.StringUtils;
+
+import static org.junit.Assert.*;
+
+public class StringUtilsTest {
+
+    StringUtils utils = new StringUtils();
+
+    @Test
+    public void testReverse() {
+        assertEquals("olleh", utils.reverse("hello"));
+        assertEquals("", utils.reverse(""));
+        assertNull(utils.reverse(null));
+    }
+
+    @Test
+    public void testIsPalindrome() {
+        assertTrue(utils.isPalindrome("madam"));
+        assertTrue(utils.isPalindrome("level"));
+        assertFalse(utils.isPalindrome("hello"));
+        assertFalse(utils.isPalindrome(null));
+    }
+
+    @Test
+    public void testToUpperCase() {
+        assertEquals("HELLO", utils.toUpperCase("hello"));
+        assertEquals("JAVA", utils.toUpperCase("java"));
+        assertEquals("", utils.toUpperCase(""));
+        assertNull(utils.toUpperCase(null));
+    }
+}
