d8eb4c2423c3d3c6ff878c638b0756882db7e39e
KrishnaSLal <krishnaslal0204@gmail.com>
2026-01-31 00:39:19 +0530

    [KrishnaSLal]: Added employee salary checking system

---
+++ java8-programming-practice/scenario-based/Java8ScenarioPractice/src/com/functionalinterface/predicate/studentsystem/employeesystem/MainEmployee.java
@@ -0,0 +1,25 @@
+//In an employee system, check if salary is greater than 30,000 using Predicate.
+
+package com.functionalinterface.predicate.studentsystem.employeesystem;
+
+class Employee {
+    String name;
+    double salary;
+
+    Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+}
+public class MainEmployee {
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("Rahul", 45000);
+        Employee e2 = new Employee("Neha", 25000);
+
+        Salary salaryCheck =e -> e.salary > 30000;
+
+        System.out.println("This employee's salary is higher than 30,000: "+ salaryCheck.check(e1));
+        System.out.println("This employee's salary is higher than 30,000: "+ salaryCheck.check(e2));
+    }
+}
---
+++ java8-programming-practice/scenario-based/Java8ScenarioPractice/src/com/functionalinterface/predicate/studentsystem/employeesystem/Salary.java
@@ -0,0 +1,6 @@
+package com.functionalinterface.predicate.studentsystem.employeesystem;
+
+@FunctionalInterface
+interface Salary {
+    boolean check(Employee employee);
+}
