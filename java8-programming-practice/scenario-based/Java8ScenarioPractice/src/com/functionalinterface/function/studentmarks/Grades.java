de52a06448c1cb3d41812bfce21a5af593f8ebeb
KrishnaSLal <krishnaslal0204@gmail.com>
2026-01-31 12:16:24 +0530

    [KrishnaSLal]: Added a program to convert student marks into grades using function

---
+++ java8-programming-practice/scenario-based/Java8ScenarioPractice/src/com/functionalinterface/function/studentmarks/Grades.java
@@ -0,0 +1,8 @@
+package com.functionalinterface.function.studentmarks;
+
+@FunctionalInterface
+
+public interface Grades {
+   
+	int gradePredictor();
+}
---
+++ java8-programming-practice/scenario-based/Java8ScenarioPractice/src/com/functionalinterface/function/studentmarks/MainStudentGrades.java
@@ -0,0 +1,36 @@
+//Convert student marks into grade (A/B/C) using Function.
+
+
+package com.functionalinterface.function.studentmarks;
+
+import java.util.Scanner;
+import java.util.function.Function;
+
+class Grade{
+	
+	double marks;
+	
+	Grade(double marks){
+		this.marks = marks;
+	}
+}
+public class MainStudentGrades {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the marks: ");
+		double marks = sc.nextDouble();
+		
+		Function<Double, String> gradePredictor = m -> {
+			if(m>=75) return "A";
+			else if(m>=50) return "B";
+			else if(m>=25) return "C";
+			else return "Fail";
+		};
+		
+		System.out.println("The grade remark is : " + gradePredictor.apply(marks));
+		
+		sc.close();
+	}
+}
