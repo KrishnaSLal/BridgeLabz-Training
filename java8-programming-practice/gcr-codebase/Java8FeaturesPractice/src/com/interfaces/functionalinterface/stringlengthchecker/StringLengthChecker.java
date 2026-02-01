bd296782ca6ab92b33e629492579123110a41870
KrishnaSLal <krishnaslal0204@gmail.com>
2026-02-01 23:50:26 +0530

    [KrishnaSLal]: Added a program to check the length of a string

---
+++ java8-programming-practice/gcr-codebase/Java8FeaturesPractice/src/com/interfaces/functionalinterface/stringlengthchecker/StringLengthChecker.java
@@ -0,0 +1,29 @@
+package com.interfaces.functionalinterface.stringlengthchecker;
+
+import java.util.function.Function;
+
+public class StringLengthChecker {
+    public static void main(String[] args) {
+        
+        int maxLimit = 10;
+
+        Function<String, Integer> stringLengthFunction = str -> str.length();
+
+        String message1 = "Hello";
+        String message2 = "Hello, Functional Interface!";
+
+        // Check messages
+        checkMessage(message1, maxLimit, stringLengthFunction);
+        checkMessage(message2, maxLimit, stringLengthFunction);
+    }
+
+    public static void checkMessage(String message, int limit, Function<String, Integer> lengthFunction) {
+        int length = lengthFunction.apply(message); 
+        if (length > limit) {
+            System.out.println("Message exceeds the limit Length: " + length);
+        } else {
+            System.out.println("Message is within the limit Length: " + length);
+        }
+    }
+}
+
