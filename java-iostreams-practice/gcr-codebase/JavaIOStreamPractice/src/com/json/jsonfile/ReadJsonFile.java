748b75aa01a044ef9937d80a331bb1ff10837fb6
KrishnaSLal <krishnaslal0204@gmail.com>
2026-02-03 15:35:08 +0530

    [KrishnaSLal]: Added a program to read a json file and extract only specific fields

---
+++ java-iostreams-practice/gcr-codebase/JavaIOStreamPractice/src/com/json/jsonfile/ReadJsonFile.java
@@ -0,0 +1,21 @@
+package com.json.jsonfile;
+
+import org.json.JSONObject;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ReadJsonFile {
+    public static void main(String[] args) throws Exception {
+
+        String content = new String(Files.readAllBytes(Paths.get("C:\\Users\\krish\\OneDrive\\Desktop\\BridgeLabz-Training\\java-iostreams-practice\\gcr-codebase\\JavaIOStreamPractice\\src\\com\\json\\jsonfile\\users.json")));
+
+        JSONObject jsonObject = new JSONObject(content);
+
+        String name = jsonObject.getString("name");
+        String email = jsonObject.getString("email");
+
+        System.out.println("Name  : " + name);
+        System.out.println("Email : " + email);
+    }
+}
+
